133739d7767cafc692443f39bfcc5af2a1df3d1e
Naftali Kulik <nkulik1994@gmail.com>
2022-07-13 15:59:03 -0400

    commit tree c2a3290d88675ae15dbf8378733dd7cf9ebbf439

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        render json: user.items
+      else
+        render json: { error: "User not found" }, status: :not_found
+      end
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
 
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item
+    else 
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      item = user.items.create(item_params)
+      render json: item, status: :created
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
